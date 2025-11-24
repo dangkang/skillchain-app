e549de1fce391c75237b2bf4f839175780fadd4b
dangkang <dgramgra@gmail.com>
2025-11-24 16:28:25 +0900

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // GitHub Pagesの場合、リポジトリ名に変更: '/skillchain-app/'
+  base: '/skillchain-app/',
 })
